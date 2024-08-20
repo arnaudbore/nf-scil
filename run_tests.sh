ab803e1d4d708ccb4db229d0bbf09bb7d4f2d4c2
AlexVCaron <alex.valcourt.caron@usherbrooke.ca>
2024-08-20 14:22:55 +0000

    QOL improvements

--- run_tests.sh
+++ run_tests.sh
@@ -1,37 +1,116 @@
 #!/usr/bin/env bash
 
-NO_RUN_TESTS=( )
+function run_test_suites () {
 
-if [ -f "failed_tests.txt" ]
-then
-rm -rf failed_tests.txt
-fi
+local component=$1
+local workdir=$2
 
-if [ -f "successful_tests.txt" ]
-then
-rm -rf successful_tests.txt
-fi
-
-for test_category in tests/modules/nf-scil/*
+for module_category in $component/nf-scil/*
 do
 
-for test_case in $test_category/*
+for module_tool in $module_category/*
 do
 
-test_instance="$(basename $test_category)/$(basename $test_case)"
+module_identifier="$(basename $module_category)/$(basename $module_tool)"
 
-if [[ ${NO_RUN_TESTS[@]} =~ $test_instance ]]
+if [[ ${EXCLUDES[@]} =~ $module_identifier ]]
 then
-echo "Skipping $test_instance"
+
+echo "Skipping $module_identifier"
+
 else
-echo "Running $test_instance"
+
+echo "Running $module_identifier"
+
+if [ -d $module_tool/tests ]
+then
+
+echo "Command : $NFCORE_COMMAND $module_identifier"
+
 {
-    nf-core modules test --no-prompts $test_instance &&
-    echo "$test_instance" >> successful_tests.txt
-} || echo "$test_instance" >> failed_tests.txt
+    declare -gx NFCORE_${component^^}_GIT_REMOTE=https://github.com/scilus/nf-scil.git
+    $NFCORE_COMMAND $module_identifier &&
+    echo "$component/$module_identifier" >> $workdir/successful_tests.txt
+} || echo "$component/$module_identifier" >> $workdir/failed_tests.txt
+
+fi
 
 fi
 
 done
 
 done
+
+echo "Ran tests for $component"
+if [ -f $workdir/failed_tests.txt ]
+then
+    echo "Failed tests:"
+    cat $workdir/failed_tests.txt
+else
+    echo "All tests were successful !"
+fi
+
+}
+
+
+DESCRIPTION="""
+
+Run all tests for all modules and subworkflows in the nf-scil library.
+
+Usage:
+    run_tests.sh [-e <exclude_test_1>] [-e <exclude_test_2>] [-u] [-o] [-p <profile>]
+
+Options:
+
+    -e <exclude_test>       Exclude a test from the test suite. Can be used multiple times.
+    -u                      Update the test snapshot after running the tests.
+    -o                      Run the tests only once, without retrying failed tests.
+    -p <profile>            Run the tests with the specified Nextflow profile.
+
+"""
+
+EXCLUDES=( )
+UPDATE_SNAPS=false
+ONCE=false
+PROFILE="docker"
+
+while getopts "e:uoph:" opt; do
+  case $opt in
+    e) EXCLUDES+=($OPTARG)
+    ;;
+    u) UPDATE_SNAPS=true
+    ;;
+    o) ONCE=true
+    ;;
+    p) PROFILE="$OPTARG"
+    ;;
+    h) echo "$DESCRIPTION"
+    ;;
+    \?) echo "Invalid option -$OPTARG" >&2
+        echo "$DESCRIPTION"
+    ;;
+  esac
+done
+
+
+TMPDIR=$(mktemp -d)
+NFCORE_COMMAND="nf-core modules test --no-prompts"
+
+if [ -n "$PROFILE" ]
+then
+    echo "adding profile"
+    NFCORE_COMMAND="$NFCORE_COMMAND --profile $PROFILE"
+fi
+
+if [ "$UPDATE_SNAPS" = true ]
+then
+    NFCORE_COMMAND="$NFCORE_COMMAND --update"
+fi
+
+if [ "$ONCE" = true ]
+then
+    NFCORE_COMMAND="$NFCORE_COMMAND --once"
+fi
+
+run_test_suites modules $TMPDIR
+run_test_suites subworkflows $TMPDIR
